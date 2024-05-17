d2abcb92056462df944864a1d152233c906ec0de
Mahesh-addagatla <maheshaddagatla1234@gmail.com>
2024-05-17 17:12:16 +0530

    first commsolu

--- src/App.js
+++ src/App.js
@@ -4,29 +4,7 @@ import "./App.css";
 import SolutionPage from "./pages/SolutionPage.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProblemsList from "./pages/ProblemsList.jsx";
-import CodeBlock from "./components/CodeBlock.jsx";
 
-const codeString = `
-class Solution{
-  // arr: input array
-  // n: size of array
-  //Function to find the sum of contiguous subarray with maximum sum.
-  long maxSubarraySum(int arr[], int n){
-    long msf=Integer.MIN_VALUE;
-    long meh=0;
-    for(int i=0;i<n;i++){
-      meh+=arr[i];
-      if (meh<arr[i]){
-        meh=arr[i];
-      }
-      if (msf<meh){
-        msf=meh;
-      }
-    }
-    return msf;
-  }
-}
-`;
 
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -39,9 +17,10 @@ function App() {
             path="/solution"
             element={<SolutionPage problemName="Valid Sudoku" />}
           ></Route>
+          <Route path="/" element={<ProblemsList/>}></Route>
         </Routes>
       </Router>
-      <ProblemsList />
+      
       {/* <CodeBlock language="java" value={codeString} /> */}
     </>
   );
