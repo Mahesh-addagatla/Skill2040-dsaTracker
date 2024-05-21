f242ef80810dc6762a6cfc620c6ffa9d3aec19c6
gvk <venkatakousikcse01@gmail.com>
2024-05-21 10:30:11 +0530

    adding all the changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Settings from './pages/settings/Settings';
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <div className="App">
+
       <Router>
 
         <Routes>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Intro from './components/intro/Intro';
 import Problems from './problems/Problems.jsx'
 import Footer from './components/footer/Footer.js';
-
+import { Analytics } from "@vercel/analytics/react"
 function Home() {
     return (
         <div className="App">
+
+            <Analytics />
             <Intro />
             <Problems />
         </div>
