a4e029b24597f1db532b8fcecbe5d62b179f9032
gvk <venkatakousikcse01@gmail.com>
2024-05-21 23:46:46 +0530

    adding all the changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,14 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Intro from './components/intro/Intro';
 import Problems from './problems/Problems.jsx'
 import Footer from './components/footer/Footer.js';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import Nav from './nav/Nav.jsx';
 function Home() {
     return (
         <div className="App">
-
             <Analytics />
-            <Intro />
-            <Problems />
+            <Nav />
+            <div className="main">
+                <Intro />
+                <Problems />
+            </div>
         </div>
     );
 }
---
+++ src/pages/home/nav/Nav.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+import './style.css'
+import { Link } from 'react-router-dom';
+import useSound from 'use-sound';
+import loud_btn from '../components/sounds/loud_btn_clk.wav';
+
+function Nav() {
+
+    const clientUrl = process.env.CLIENT_URL;
+
+    const [play] = useSound(loud_btn);
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    return (
+        <>
+            <div className='navbar-container '>
+                <div className="NavContainer">
+                    <div className='logo'>
+                        <img src="/images/nav/logo.jpg"></img>
+                        <h4>DSA-Tracker</h4>
+                    </div>
+                    <nav className='fill stroke'>
+
+                        <li>
+                            <Link to={clientUrl} className='active' onClick={play}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={clientUrl} onClick={play}>
+                                About
+                            </Link>
+                        </li>
+                        <li className='dropdown'>
+                            <Link to={clientUrl} onClick={play}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='dropdown'>
+                            <Link to={clientUrl} onClick={play}>
+                                CaseStudies
+                            </Link>
+                        </li>
+
+
+                    </nav>
+                    <div className='profile' onClick={play}>
+                        <span className='name'>G.VenkataKousik</span>
+                        <img src="/images/nav/gvk.jpg"></img>
+                    </div>
+                </div>
+
+            </div >
+
+            <nav className='nav-responsive'>
+                {showMenu && (
+                    <ul className='responsive-menu' >
+                        <div className='tags show'>
+                            <li>
+                                <Link to={clientUrl} className='active' onClick={play}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={clientUrl} onClick={play}>
+                                    About
+                                </Link>
+                            </li>
+                            <li className='dropdown'>
+                                <Link to={clientUrl} onClick={play}>
+                                    Services
+                                </Link>
+                            </li>
+                            <li className='dropdown'>
+                                <Link to={clientUrl} onClick={play}>
+                                    CaseStudies
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={clientUrl} onClick={play}>
+                                    Specialities
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={clientUrl} onClick={play}>
+                                    Contact Us
+                                </Link>
+                            </li>
+                        </div>
+                    </ul>
+
+                )}
+
+            </nav>
+        </>
+
+    )
+}
+
+export default Nav
