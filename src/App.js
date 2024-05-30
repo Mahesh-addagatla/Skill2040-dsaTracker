0f73fc2cccabc07b7242c98f7bcc5ea43db4136b
Surya-0804 <surya.abothula@gmail.com>
2024-05-30 14:55:01 +0530

    adding contribution board

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Settings from './pages/settings/Settings';
 import { Analytics } from "@vercel/analytics/react"
+import ContributionBoard from './pages/contibutionboard/ContributionBoard';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/contribution" element={<ContributionBoard/>}/>
         </Routes>
 
       </Router>
