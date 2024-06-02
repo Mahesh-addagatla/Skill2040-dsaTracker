569b60fd367fa7919cd367b91d3a5e60ee24b167
gvk <venkatakousikcse01@gmail.com>
2024-06-02 21:49:27 +0530

    adding all the changes

--- src/pages/solution/Solution.jsx
+++ src/pages/solution/Solution.jsx
@@ -1,19 +1,17 @@
 import './style.css'
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Intro from './components/intro/Intro';
-import Problems from './problems/Problems.jsx'
 import Footer from './components/footer/Footer.jsx';
 import { Analytics } from "@vercel/analytics/react";
 import Nav from './nav/Nav.jsx';
+import SolutionPage from '../solutionpage/SolutionPage.jsx';
 function Home() {
     return (
         <div className="App">
             <Analytics />
             <Nav />
             <div className="main">
-                <Intro />
-                <Problems />
+                <SolutionPage />
             </div>
             <Footer />
         </div>
